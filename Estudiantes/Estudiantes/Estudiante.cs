c44cafa0cff81effd07df74c293c4738b9953f49
Marvin Campos <mc274426@hotmail.com>
2019-08-19 11:14:21 -0600

    Agregar archivos de proyecto.

---
+++ Estudiantes/Estudiantes/Estudiante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estudiantes
+{
+  public   class Estudiante
+    {
+        public string ID { get; set; }
+        public string NombreEstudiante { get; set; }
+        public string ApellidoEstudiante { get; set; }
+        public string Curso { get; set; }
+        public int Edad { get; set; }
+        public string Direccion { get; set; }
+        public string ResponsableEmergencia { get; set; }
+
+    }
+}
